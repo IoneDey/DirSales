c037e1376e4a011b7f690241c1ff246a9d0e7878
Dedy Setiawan <ione.dey@gmail.com>
2024-04-09 13:05:19 +0700

    paginate

--- app/Livewire/Panel/Pt/Index.php
+++ app/Livewire/Panel/Pt/Index.php
@@ -3,14 +3,58 @@
 namespace App\Livewire\Panel\Pt;
 
 use Livewire\Component;
+use App\Models\Pts as ModelsPT;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
     public $title = 'Panel - PT';
+
+    public $kode;
+    public $nama;
+    public $angsuranhari;
+    public $angsuranperiode;
+
+    public function store()
+    {
+        $rules = [
+            'kode' => ['required', 'min:3', 'max:10', 'unique:pts'],
+            'nama' => ['required', 'min:5', 'max:255'],
+            'angsuranhari' => ['required', 'integer', 'min:3', 'max:31'],
+            'angsuranperiode' => ['required', 'integer', 'min:3', 'max:12']
+        ];
+        $pesan = [
+            'kode.required' => 'Kode wajib diisi.',
+            'nama.required' => 'Nama wajib diisi.',
+            'angsuranhari.required' => 'Angsuran-Hari wajib diisi.',
+            'angsuranperiode.required' => 'Angsuran-Peride wajib diisi.',
+            'kode.min' => 'Kode min 3 karakter.',
+            'nama.min' => 'Nama min 5 karakter.',
+            'kode.max' => 'Kode maximum 10 karakter',
+            'nama.max' => 'Nama maximum 255 karakter',
+            'angsuranhari.min' => 'Angusran min 3',
+            'angsuranperiode.min' => 'Angusran min 3',
+            'angsuranhari.max' => 'Angusran max 31',
+            'angsuranperiode.max' => 'Angusran max 12',
+            'kode.unique' => 'Kode sudah ada didata.',
+        ];
+
+
+        $validatedData = $this->validate($rules, $pesan);
+        $validatedData['userid'] = 1;
+        //$validatedData['userid'] = auth()->user()->id;
+        ModelsPT::create($validatedData);
+        session()->flash('ok', 'Data ' . $this->kode . ' berhasil disimpan.');
+    }
+
 
     public function render()
     {
-        return view('livewire.panel.pt.index')->layout('layouts.panel-layout', [
+        $data = ModelsPT::orderBy('updated_at', 'desc')->paginate(7);
+        return view('livewire.panel.pt.index', [
+            'dataPT' => $data,
+        ])->layout('layouts.panel-layout', [
             'title' => $this->title,
         ]);
     }
--- resources/views/livewire/panel/pt/index.blade.php
+++ resources/views/livewire/panel/pt/index.blade.php
@@ -1,18 +1,42 @@
 <div>
+    @if(session()->has('ok'))
+    <div class="alert alert-danger" id="ok-message">
+        {{ session('ok') }}
+    </div>
+    <script>
+        setTimeout(function() {
+            var errorMessage = document.getElementById('ok-message');
+            if (errorMessage) {
+                errorMessage.remove();
+            }
+        }, 3500);
+    </script>
+    @endif
+
     <div class='container'>
         <form class="shadow-lg p-3 mb-1" action="">
-
+            @csrf
             <div class="row">
                 <div class="col-sm-6 col-md-4">
                     <div class="form-floating mb-2">
-                        <input autofocus type="email" class="form-control" id="floatingInput1" placeholder=" ">
+                        <input wire:model="kode" autofocus type="text" class="form-control @error('kode') is-invalid @enderror" id="floatingInput1" placeholder=" ">
                         <label for="floatingInput1">Kode</label>
+                        @error('kode')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div class="form-floating mb-1">
-                        <input type="email" class="form-control" id="floatingInput2" placeholder=" ">
+                        <input wire:model="nama" type="text" class="form-control @error('nama') is-invalid @enderror" id="floatingInput2" placeholder=" ">
                         <label for="floatingInput2">Nama</label>
+                        @error('nama')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -20,24 +44,35 @@
             <div class="row">
                 <div class="col-sm-6 col-md-4">
                     <div class="form-floating mb-2">
-                        <input type="email" class="form-control" id="floatingInput3" placeholder=" ">
+                        <input wire:model="angsuranhari" type="number" class="form-control @error('angsuranhari') is-invalid @enderror" id="floatingInput3" placeholder=" ">
                         <label for="floatingInput3">Angsuran - Hari</label>
+                        @error('angsuranhari')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div class="form-floating mb-2">
-                        <input type="email" class="form-control" id="floatingInput4" placeholder=" ">
+                        <input wire:model="angsuranperiode" type="number" class="form-control @error('angsuranperiode') is-invalid @enderror" id="floatingInput4" placeholder=" ">
                         <label for="floatingInput4">Angsuran - Periode</label>
+                        @error('angsuranperiode')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
-                    <button class="btn btn-primary" type="submit">Simpan</button>
+                    <button type="button" name='btnsimpan' class="btn btn-primary" wire:click="store()">Simpan</button>
                 </div>
             </div>
         </form>
 
         <form class="shadow-lg p-3 mb-1" action="">
             <div class="col-sm-12">
+                {{ $dataPT->links() }}
                 <table class="table table-sm table-striped table-hover">
                     <thead>
                         <tr>
@@ -50,86 +85,22 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ( $dataPT as $pt )
                         <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $pt->kode }}</td>
+                            <td>{{ $pt->nama }}</td>
+                            <td class="rata-kanan">{{ $pt->angsuranhari }}</td>
+                            <td class="rata-kanan">{{ $pt->angsuranperiode }}</td>
+                            <td class="rata-kanan">
+                                <a class="badge bg-warning" href="#"><i class="bi bi-pencil-fill"></i></a>
+                                <form action="" method="POST" style="display: inline;">
+                                    <button type="submit" class="badge bg-danger border-0" onclick="return confirm('Apakah Anda yakin ingin menghapus data?')"><i class="bi bi-eraser"></i></button>
+                                </form>
+                            </td>
                         </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>kode</td>
-                            <td>nama</td>
-                            <td>angsuran1</td>
-                            <td>angsuran2</td>
-                            <td>act</td>
-                        </tr>
+                        @endforeach
+
 
                     </tbody>
                 </table>
